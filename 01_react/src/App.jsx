af0988103a3259225fd746e4cae1932ec81507fc
SK sanoyar ali <asksanoyar33@gmail.com>
2025-05-21 13:16:59 +0530

    01_react done

---
+++ 01_react/src/App.jsx
@@ -0,0 +1,13 @@
+
+import Chai from "./Chai";
+function App() {
+  const userName=" by hiteshdotcom";
+  return (
+    <>
+    <Chai/>
+    <h1>Chai aur react with Vite{userName}</h1>
+    </>
+  )
+}
+
+export default App;
---
+++ 01_react/src/Chai.jsx
@@ -0,0 +1,6 @@
+function Chai(){
+    return (
+        <h1>This is Chai</h1>
+    )
+}
+export default Chai;
